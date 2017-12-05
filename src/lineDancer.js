aa224f74533e4ccf0a2ebc909fb319bbfbb61578
mishroom <mishall.afzal@gmail.com>
2017-12-05 12:18:29 -0800

    yoda complete

--- src/lineDancer.js
+++ src/lineDancer.js
@@ -2,7 +2,7 @@ var StormTrooper = function(top, left, timeBetweenSteps) {
   Dancer.call(this, top, left, timeBetweenSteps);
   this.$node.addClass('line');
   this.setPosition(top, left);
-  this.$node.html('<img src = "stormtrooper_head.png" height="50px" width="50px">');
+  this.$node.html('<img src = "stormtrooper_head.png" height="100px">');
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
 
---
+++ src/yoda.js
@@ -0,0 +1,29 @@
+var Yoda = function(top, left, timeBetweenSteps) {
+  Dancer.call(this, top, left, timeBetweenSteps);
+  this.$node.addClass('yoda');
+  this.setPosition(top, left);
+  this.$node.html('<img src = "Master_Yoda.png" height="130px">');
+  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+  // so we must keep a copy of the old version of this function
+
+
+};
+
+Yoda.prototype = Object.create(Dancer.prototype);
+Yoda.prototype.constructor = Yoda;
+Yoda.prototype.setPosition = function(top, left) {
+  var styleSettings = {
+    top: top,
+    right: left
+  };
+  this.$node.css(styleSettings);
+};
+Yoda.prototype.step = function() {
+  Dancer.prototype.step.call(this);
+  this.$node.animate({
+   //top: top
+    //left: [ 'toggle']
+    "right": 'toggle'}, "slow"
+  );
+};
+
